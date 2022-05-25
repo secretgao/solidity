4331e8c687adb10bb2a4d3ebf2d770f5c601cd75
secretgao <891841626@qq.com>
2022-05-25 10:48:02 +0800

    Update AccessControl.sol

--- AccessControl.sol
+++ AccessControl.sol
@@ -36,3 +36,20 @@ contract AccessControl{
         emit RevokRole(_role,_account);
     }
 }
+/*
+验证流程 
+1 先把admin和user变成public部署合约，点击admin和user按钮 ，获取hash之后的值，在变成private，部署合约 
+得到 admin的哈希值：0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42
+user的哈希值：0x2db9fd3d099848027c2383d0a083396f6c41510d7acfd92adc99b6cffcf31e96
+2 把admin 和user的public 改成private 部署合约 
+3 会出现 grantRole（） revokRole() roles（） 三个方法
+4 因为合约写了构造函数，部署者拥有admin权限  验证部署者是不是管理员  在
+roles（admin的哈希值，部署者账号） 返回true 证明构造函数生效
+5 升级账户 
+  在ACCOUNT账户列表 选择另一个账户复制
+  调用grantRole(user的哈希值,另一个账号)；//当前操作账户必须是部署账号 调用成功之后
+6 调用roles  grantRole(user的哈希值,另一个账号) //返回true 说明设置生效
+7 撤销账户
+  调用revokRole(user的哈希值,另一个账号)；//当前操作账户必须是部署账号 调用成功之后
+8 调用roles  grantRole(user的哈希值,另一个账号) //返回false 说明设置生效
+*/
